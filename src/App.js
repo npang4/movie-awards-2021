c519420f9715aec7ebcee575b0031936c0cfc8fe
Nelson Pang <nelsoncpang@gmail.com>
2022-07-01 12:46:40 -0700

    worked on frontend challenge

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import './App.css';
 import Ballot from './Components/Ballot/Ballot';
+import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
   // Feel free to remove the contents of the header tag to make more room for your code
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
+      <h1 className='award-title'> Awards 2021</h1>
       <Ballot />
     </div>
   );
--- src/Components/Ballot/Ballot.js
+++ src/Components/Ballot/Ballot.js
@@ -1,11 +1,64 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Modal from 'react-modal';
+import "./Ballot.css";
+import 'bootstrap/dist/css/bootstrap.css';
 
 const Ballot = () => {
+
+  const [data, setData] = useState([]);
+  const [modalIsOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    fetch(`/api/getBallotData`)
+      .then(response => {
+        return response.json();
+      })
+      .then(actualData => {
+        console.log(actualData);
+        setData(actualData);
+      })
+      .catch(e => {
+        console.log(e);
+      })
+  })
+
+  const customStyles = {
+    content: {
+      top: '40%',
+      left: '35%',
+      right: 'auto',
+      bottom: 'auto',
+    }
+  }
+
   return (
     <div className='ballot'>
-      'Your Code Goes Here'
+      {data.items?.map((item) => (
+        <div className='ballot-info'>
+          <h1 className='nominee-title'>{item.title}</h1>
+          {item.items.map((insideItem) => (
+            <div id='movie-info' >
+
+              <h3>{insideItem.title}</h3>
+              <img className={insideItem.id} src={insideItem.photoUrL} />
+              <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1"></input>
+            </div>
+          ))}
+        </div>
+      ))}
+
+      <button id='submit-button' onClick={() => setIsOpen(true)} > Submit Votes!</button>
+      <Modal isOpen={modalIsOpen} onRequestClose={() => setIsOpen(false)} style={customStyles}>
+        <div>
+          <h2> Your votes have been submitted!</h2>
+        </div>
+
+      </Modal>
+
+
     </div>
   )
 }
+
 
 export default Ballot;
